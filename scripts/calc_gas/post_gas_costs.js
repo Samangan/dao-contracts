f57ea30f0d11f4ddcddec19de1fbe26e5f52fca9
Harry Hull <harry.hull1@gmail.com>
2022-04-17 16:17:52 -0700

    Refactor js script

--- scripts/calc_gas/post_gas_costs.js
+++ scripts/calc_gas/post_gas_costs.js
@@ -2,29 +2,7 @@ const fs = require('fs');
 
 module.exports = async ({ github, context, core }) => {
   const gasUsage = getGasUsage();
-
-  const commentHeader = ```
-    ![gas](https://liquipedia.net/commons/images/thumb/7/7e/Scr-gas-t.png/20px-Scr-gas-t.png) 
-        ~ Gas Diff Report ~ 
-    ![gas](https://liquipedia.net/commons/images/thumb/7/7e/Scr-gas-t.png/20px-Scr-gas-t.png)
-  ```;
-
-  var commentData = "";
-  for (var contract in gasUsage) {
-    commentData += `  * ${contract}:` + '\n';
-
-    for (var f in gasUsage[contract]) {
-      const mainUsage = gasUsage[contract][f]["main"];
-      const prUsage = gasUsage[contract][f]["pr"];
-
-      commentData += `    * ${f}:` + '\n';
-      commentData += `      * Change:` + '\n';
-      commentData += `      * main: ${github.event.pull_request.base.sha}: ${mainUsage} ` + '\n';
-      commentData += `      * PR: ${github.sha}: ${prUsage}` + '\n\n';
-    }
-  }
-
-  const commentBody = commentHeader + '\n' + commentData;
+  const commentBody = buildComment(gasUsage);
 
   const { data: comments } = await github.rest.issues.listComments({
     issue_number: context.issue.number,
@@ -53,30 +31,30 @@ module.exports = async ({ github, context, core }) => {
 
 function getGasUsage() {
   var gasUsage = {};
-  fs.readdir("./gas_usage/", function (err, contract_dirs) {
+  fs.readdir("./gas_usage/", function (err, contractDirs) {
     if (err) {
       console.error("Error reading gas_usage dir:", err);
       return
     }
 
-    contract_dirs.forEach(function (contract_dir, index) {
-      console.log("Processing: " + contract_dir);
+    contractDirs.forEach(function (contractDir, index) {
+      console.log("Processing: " + contractDir);
 
-      fs.readdir(`./gas_usage/${contract_dir}`, function (err, files) {
+      fs.readdir(`./gas_usage/${contractDir}`, function (err, files) {
         if (err) {
-          console.error("Error reading " + contract_dir, err);
+          console.error("Error reading " + contractDir, err);
           return
         }
 
         files.forEach(function (file, index) {
           console.log("Loading: " + file);
-          fs.readFile(`./gas_usage/${contract_dir}/${file}`, 'utf8', (err, data) => {
+          fs.readFile(`./gas_usage/${contractDir}/${file}`, 'utf8', (err, data) => {
             if (err) {
               console.error(err)
               return
             }
             console.log(data)
-            gasUsage[contract_dir][file] = data;
+            gasUsage[contractDir][file] = JSON.parse(data);
           });
         });
       });
@@ -85,4 +63,30 @@ function getGasUsage() {
 
   console.log(gasUsage);
   return gasUsage;
+}
+
+function buildComment(gasUsage) {
+  const commentHeader = ```
+    ![gas](https://liquipedia.net/commons/images/thumb/7/7e/Scr-gas-t.png/20px-Scr-gas-t.png) 
+        ~ Gas Diff Report ~ 
+    ![gas](https://liquipedia.net/commons/images/thumb/7/7e/Scr-gas-t.png/20px-Scr-gas-t.png)
+  ```;
+
+  var commentData = "";
+  for (var contract in gasUsage) {
+    commentData += `  * ${contract}:` + '\n';
+
+    for (var f in gasUsage[contract]) {
+      const mainUsage = gasUsage[contract][f]["main"];
+      const prUsage = gasUsage[contract][f]["pr"];
+      const pctChange = (prUsage - mainUsage) / mainUsage * 100;
+
+      commentData += `    * ${f}:` + '\n';
+      commentData += `      * Change: ${pctChange}%` + '\n';
+      commentData += `      * main: ${github.event.pull_request.base.sha}: ${mainUsage} ` + '\n';
+      commentData += `      * PR: ${github.sha}: ${prUsage}` + '\n\n';
+    }
+  }
+
+  return commentHeader + '\n' + commentData;
 }
